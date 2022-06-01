0a136c5df10ab0f5ae630377581b41ff11964415
Muhammad Syibli <muhammadsyibli@live.com>
2022-06-01 22:36:53 +0700

    adding login function for multiple guard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,29 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:merchant')->except('logout');
+    }
+
+    public function showMerchantLoginForm()
+    {
+        return view('auth.login', ['url' => 'merchant']);
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if(Auth::guard('merchant')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember_token'))) {
+            return redirect()->intended('/merchant');
+        }
+
+        if(Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember_token'))) {
+            return redirect()->intended('/home');
+        }
+
+        return back()->withInput($request->only('email', 'remember'));
     }
 }
